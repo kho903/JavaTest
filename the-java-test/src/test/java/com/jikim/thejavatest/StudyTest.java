6e73675f6d53f982fed4647cb6c93fc63521f62c
kho903 <gmldnr2222@naver.com>
2022-04-11 00:28:05 +0900

    feat: 01. Junit / 03. 테스트 이름 표시하기

--- the-java-test/src/test/java/com/jikim/thejavatest/StudyTest.java
+++ the-java-test/src/test/java/com/jikim/thejavatest/StudyTest.java
@@ -4,17 +4,19 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.*;
 
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class) // _ -> ""
 class StudyTest {
 
 	@Test
-	void create() {
+	@DisplayName("스터디 만들기")
+	void create_new_study() {
 		Study study = new Study();
 		assertNotNull(study);
 		System.out.println("create");
 	}
 
 	@Test
-	void create1() {
+	void create_new_study_again() {
 		System.out.println("create1");
 	}
 
